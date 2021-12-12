5fd9759034513493a5aca23b5ae3acb4ffe79121
rkaur21 <57161170+rkaur21@users.noreply.github.com>
2021-12-11 16:55:37 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,93 +1,47 @@
-class Exercise_1 {
-  public static void main(String args[])
-  {
+class Stack { 
+  
+    //Please read sample.java file before starting.
+  //Kindly include Time and Space complexity at top of each file
+    static final int MAX = 1000; 
+    int top; 
+    int a[] = new int[MAX]; // Maximum size of Stack 
+  
+    boolean isEmpty() 
+    { 
+        //Write your code here 
+    } 
 
-
-
- 
-
-
-
- 
-class Stack {
- 
-  static final int MAX = 10;
-  int top;
-
-  int [] Stack;// Maximum size of Stack
-
- public boolean isEmpty()
-  {
-     return top == -1;
-      //Write your code here
-  }
-    public  boolean isFull()
-  {
-       return top == MAX -1;
-      //Write your code here
-  }
-   
-
-  Stack()
-  {
- 
-      top=-1;//Initialize your constructor
-      Stack = new int[MAX];
-  }
-
-public  boolean push(int x)
-  {
-      if(isFull())
-      {
-          System.out.println("Stack is full");
-      }
-      Stack[++top] = x;
-     return true;
-}      
-           //Check for stack Overflow
-      //Write your code her
-  int pop()
-  {
-      if(isEmpty()){
-          System.out.println("Stack is empty");
-      }
-
-    return  Stack[top--];
-  }  
-     
-      //If empty return 0 and print " Stack Underflow"
-      //Write your code here
- 
- 
-
-public  int peek()
-  {
-   
-      return Stack[top];      //Write your code here
-  }
-    // public void Desplay(){
-    //     for (int i : Stack)
-    //     {
-    //         System.out.println(i);
-    //     }
-//}
-    //testing
-}
-
-      Stack s = new Stack();
-      s.push(10);
-      s.push(20);
-      s.push(30);
-       s.push(40);
-       s.push(50);
-       s.push(60);
-       s.push(70);
-     
-      System.out.println(s.isEmpty());
-      System.out.println(s.isFull());
-      System.out.println(s.peek() + " top from stack");
- System.out.println(s.pop() + " Popped from stack");
-     // s.Desplay();
-
-  }
+    Stack() 
+    { 
+        //Initialize your constructor 
+    } 
+  
+    boolean push(int x) 
+    { 
+        //Check for stack Overflow
+        //Write your code here
+    } 
+  
+    int pop() 
+    { 
+        //If empty return 0 and print " Stack Underflow"
+        //Write your code here
+    } 
+  
+    int peek() 
+    { 
+        //Write your code here
+    } 
+} 
+  
+// Driver code 
+class Main { 
+    public static void main(String args[]) 
+    { 
+        Stack s = new Stack(); 
+        s.push(10); 
+        s.push(20); 
+        s.push(30); 
+        System.out.println(s.pop() + " Popped from stack"); 
+    } 
 }
